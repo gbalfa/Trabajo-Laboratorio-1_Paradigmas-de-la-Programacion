24984ac864a1a9db26a7a21d8663bdff15b28848
Gabriel Badilla <gabriel.badilla.alfaro@gmail.com>
2019-05-15 15:02:27 -0400

    Documentación y se añade librería para medir el tiempo.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 import match
 import visual
 
@@ -6,8 +7,8 @@ def puntoEntremedio(P, index_p1, index_p2):
     """Función que verifica si hay puntos dentro de el rectángulo formado entre los puntos p1 y p2.
 
     :param P: Puntos ordenados segun su coordenada x de menor a mayor.
-    :param index_p1: Index punto en P.
-    :param index_p2: Index punto en P.
+    :param index_p1: Index punto p1 en P.
+    :param index_p2: Index punto p2 en P.
     :returns:  True si es que hay puntos dentro y False si no.
     :rtype: Bool.
 
@@ -17,7 +18,18 @@ def puntoEntremedio(P, index_p1, index_p2):
             return True
     return False
 
+
 def busquedaInversa(L, x):
+    """Función que busca el elemento x en una lista ordenada L de atrás hacia adelante.
+
+    Es más probable que el elemento x sea mayor, por lo tanto, la búsqueda es de adelante hacia atrás.
+
+    :param L: Lista ordenada de menor a mayor.
+    :param x: Index.
+    :returns: True si el elemento está presente en la lista, False si no.
+    :rtype: Bool.
+
+    """
     largoL = len(L)
     i = -1
     while(-largoL <= i and x <= L[i]):
@@ -25,22 +37,44 @@ def busquedaInversa(L, x):
             return True
         i -= 1
     return False
-    
+
+
 def insercionOrdenada(L, x):
+    """Función que inserta el elemento x de forma ordenada(menor a mayor) en L, basado en el algoritmo insertion sort.
+
+    Es más probable que el elemento x sea mayor, por lo tanto, la búsqueda del lugar de inserción es de adelante hacia atrás.
+
+    :param L: Lista ordenada de menor a mayor.
+    :param x: Index.
+    :returns: Modifica la misma lista.
+    :rtype: void.
+
+    """
     L.append(x)
     largoL = len(L)
     if largoL == 1:
-        return 
+        return
     i = -2
     while(-largoL <= i and x < L[i]):
         L[i+1] = L[i]
         i = i - 1
     L[i+1] = x
-    return 
-
+    return
 
 
 def interseccionRectangulos(R, p1, p2):
+    """Función que verifica si el rectángulo que formaría p1 y p2 no se intersecta con rectángulos previamente creados.
+
+    Es más probable que el rectángulo formado entre p1 y p2 se intersecte con los rectángulos creados recientemente, por lo tanto,
+    la búsqueda se realiza de adelante hacia atrás.
+
+    :param R: Lista de rectángulos.
+    :param p1: Punto.
+    :param p2: Punto.
+    :returns: True si hay interseccion de rectángulos, False si no.
+    :rtype: Bool.
+
+    """
     largoR = len(R)
     if largoR == 0:
         return False
@@ -59,11 +93,10 @@ def heuristica(P):
     P.sort(key=lambda x: x.x)
     R = []
     indexPuntosApareados = []
-    i = 0
     largoP = len(P)
     for i in range(largoP):
-        p1 = P[i]
         if (not busquedaInversa(indexPuntosApareados, i)):
+            p1 = P[i]
             for j in range(i+1, largoP):
                 p2 = P[j]
                 if (p1.color == p2.color) and (not busquedaInversa(indexPuntosApareados, j)) and (not puntoEntremedio(P, i, j)) and (not interseccionRectangulos(R, p1, p2)):
@@ -73,9 +106,12 @@ def heuristica(P):
     return R
 
 
-n = 10000
+n = 100
 print(n)
 P = match.create_random_points(n)
+start = time.time()
 R = heuristica(P)
-porcentaje = (str(len(R)*2/n*100) + "%")
+end = time.time()
+print(end - start, " segundos")
+porcentaje = str(n) + " puntos:  " + (str(len(R)*2/n*100) + "%")
 visual.Window(P, R, porcentaje)
